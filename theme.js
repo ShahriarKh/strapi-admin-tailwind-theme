e5dd1adcae246c414e81c273a89e494fec67815c
Shahriar <31452340+ShahriarKh@users.noreply.github.com>
2023-08-06 13:29:50 +0330

    Update darkest color

--- theme.js
+++ theme.js
@@ -285,6 +285,26 @@ const rose = {
   c950: "#4c0519",
 };
 
+function darkenColor(color, amount = 0.5) {
+  color = color.replace("#", "");
+
+  const red = parseInt(color.substring(0, 2), 16);
+  const green = parseInt(color.substring(2, 4), 16);
+  const blue = parseInt(color.substring(4, 6), 16);
+
+  const darkenedRed = Math.round(red * (1 - amount));
+  const darkenedGreen = Math.round(green * (1 - amount));
+  const darkenedBlue = Math.round(blue * (1 - amount));
+
+  const darkenedColor = `#${darkenedRed
+    .toString(16)
+    .padStart(2, "0")}${darkenedGreen
+    .toString(16)
+    .padStart(2, "0")}${darkenedBlue.toString(16).padStart(2, "0")}`;
+
+  return darkenedColor;
+}
+
 const darkMapper = {
   // strapi: tailwind
   0: undefined,
@@ -339,10 +359,11 @@ const createColors = (color, name, isDarkTheme = true) => {
     // primary, secondary, alternative, warning, success, danger
     weights = ["100", "200", "500", "600", "700"];
   }
+
   weights.map(
     (weight) =>
       (obj[`${name}${weight}`] = isDarkTheme
-        ? color[`c${darkMapper[weight]}`] || "black"
+        ? color[`c${darkMapper[weight]}`] || darkenColor(color.c950)
         : color[`c${lightMapper[weight]}`] || "white")
   );
   return obj;
